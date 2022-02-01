c2e811583030523633b6aa2812a67cb675332fce
Marcus Vinicius <18752038+marcusvinic2@users.noreply.github.com>
2022-02-01 08:39:11 -0300

    adicionado autenticação com token jwt

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,7 @@
 import * as Yup from 'yup'
 import User from '../models/User'
+import jwt from 'jsonwebtoken'
+import authConfig from '../../config/auth'
 
 class SessionController {
 	async store(request, response){
@@ -26,7 +28,15 @@ class SessionController {
 			return response.status(401).json({ error: 'Email ou senha incorretos'})
 		}
 
-		return response.json({ id: user.id, email, name: user.name, admin: user.admin })
+		return response.json({ 
+			id: user.id, 
+			email, 
+			name: user.name, 
+			admin: user.admin, 
+			token: jwt.sign({ id: user.id }, authConfig.secret, {
+				expiresIn: authConfig.expiresIn,
+			}) 
+		})
 	}
 }
 
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+	secret: 'codificacao',
+	expiresIn: '5d'
+}
