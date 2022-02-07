7ed228683c8eaf9a82ba6c456df82ec1753dfc86
Marcus Vinicius <18752038+marcusvinic2@users.noreply.github.com>
2022-02-07 15:10:07 -0300

    conexão com mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -3,17 +3,30 @@ import User from '../app/models/User'
 import Product from '../app/models/Product'
 import Category from '../app/models/Category'
 import configDatabase from '../config/database'
+import mongoose from 'mongoose'
 
 const models = [User, Product, Category]
 
 class Database {
 	constructor(){
 		this.init()
+		this.mongo()
 	}
 	init(){
 		this.connection = new Sequelize(configDatabase)
 		models.map(model => model.init(this.connection)).map( model => model.associate && model.associate(this.connection.models))
 	}
+
+	mongo(){
+		this.mongoConnection = mongoose.connect(
+			'mongodb://153.92.214.229:27017/mongo-uppedidos',
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			}
+			)
+	}
+
 }
 
 
